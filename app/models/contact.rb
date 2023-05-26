917b524545132b48165d2cef55501315c5ce2dbd
Guilherme Castello Clear <guilherme.castello.clear@gmail.com>
2023-05-26 13:01:23 -0300

    Feat: Using POST with belongs_to

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,11 +1,14 @@
 class Contact < ApplicationRecord
-    belongs_to :kind
+    belongs_to :kind, optional: true
     
-    def author
-        "Castello"
-    end
+   # def author
+   #     "Castello"
+   # end
 
-    def as_json(options={})
-        super(methods: :author, root: true)
-    end
+   # def as_json(options={})
+   #     super(root: true, methods: [:author, :kind_description])
+   #end
+   # def kind_description
+   #     self.kind.description
+   # end
 end

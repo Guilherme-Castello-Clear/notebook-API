8b4475739f1e02a10a8e0f8bf14bb18e1e1c08eb
Guilherme Castello Clear <guilherme.castello.clear@gmail.com>
2023-05-29 08:17:12 -0300

    Feat: Added I18n

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,6 +1,18 @@
 class Contact < ApplicationRecord
     belongs_to :kind, optional: true
-    
+
+    def birthdate_br
+        I18n.l(self.birthdate) unless self.birthdate.blank?
+    end
+
+    def to_br
+        {
+            name: self.name,
+            email: self.email,
+            birthdate: (I18n.l(self.birthdate) unless self.birthdate.blank?),
+            
+        }
+    end
    # def author
    #     "Castello"
    # end
@@ -11,4 +23,13 @@ class Contact < ApplicationRecord
    # def kind_description
    #     self.kind.description
    # end
+
+
+   def hello
+    I18n.t('hello')
+   end
+
+   def i18n
+    I18n.default_locale
+   end
 end
